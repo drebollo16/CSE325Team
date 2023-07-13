8b14257057d13d657f541471bc7673bb51e449e6
Denise Rebollo <drebollo16@byui.edu>
2023-07-13 12:12:47 -0600

    CSS Changes

--- SacramentMeeting/SacramentMeeting/Controllers/HomeController.cs
+++ SacramentMeeting/SacramentMeeting/Controllers/HomeController.cs
@@ -23,6 +23,12 @@ namespace SacramentMeeting.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
--- SacramentMeeting/SacramentMeeting/Models/MeetingPlanner.cs
+++ SacramentMeeting/SacramentMeeting/Models/MeetingPlanner.cs
@@ -8,12 +8,19 @@ namespace SacramentMeeting.Models
 { 
     public class MeetingPlanner
     {
+        [Required]
         public int ID { get; set; }
         [Display(Name = "Meeting Date")]
         public DateTime MeetingDate { get; set; }
         [Display(Name = "Opening Prayer")]
+        [StringLength(50)]
+
+
         public string OpeningPrayer { get; set; }
         [Display(Name = "Conducting Leader")]
+        [Required] 
+        [StringLength(50, ErrorMessage = "Cannot be longer than 50 characters.")]
+
         public string ConductingLeader { get; set; }
         [Display(Name = "Opening Song")]
         public string OpeningSong { get; set; }
